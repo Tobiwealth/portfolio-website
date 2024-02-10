cf578604d2c9e1876c29232b311d31bc10b24a82
Oluwatobi Oloro <Oluwatobi Oloro>
2024-02-10 23:52:25 +0100

    updated portfolio projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -78,7 +78,7 @@ const Projects = () => {
 		        variants={quickViewVariants}
 		        initial="hidden"
 		        animate="closed"
-		        className="font-tektur text-3xl md:text-6xl text-sky-white bg-cool-black font-bold text-center flex justify-center items-center w-full h-screen absolute top-16 bottom-0 right-0 left-0"
+		        className="font-tektur text-3xl md:text-6xl text-sky-white pb-20 bg-cool-black font-bold text-center flex justify-center items-center w-full h-screen absolute top-16 bottom-0 right-0 left-0"
 		    >Projects</motion.div>
 		    <motion.div 
 		        variants={quickCloseVariants}
@@ -105,6 +105,90 @@ const Projects = () => {
 					    <a target="_blank" rel="noreferrer" href="https://ehya-ecommerce.vercel.app/">
 					        <h2 className="font-montserrat text-2xl md:text-3xl lg:text-5xl">Ehya Store</h2>
 					        <p className="font-dmsans text-lg md:text-xl mt-2 ">An e-comerce website to purchase your digital wearables and other devices</p>
+					    </a>
+					</motion.div>
+				</motion. div>
+				<motion.div 
+				    variants= {containerVariants}
+				    initial="hidden"
+				    animate="visible"
+				    whileHover="hover"
+				    whileInView="scroll"
+				    className="text-center flex justify-center items-center text-center rounded-lg h-[300px] lg:h-[400px] w-[300px] lg:w-[500px] bg-quil bg-center bg-no-repeat bg-cover"
+				>
+					<motion.div
+					    variants={hoverVariants}
+					    initial="hidden"
+				        whileHover="hover"
+				        className="p-4 w-full h-full grid place-items-center text-sky-white font-bold"
+				        style={{backdropFilter: 'blur(10px)'}}
+					>
+					    <a target="_blank" rel="noreferrer" href="https://quil-web-3z1h.vercel.app/">
+					        <h2 className="font-montserrat text-2xl md:text-3xl lg:text-5xl">Quil</h2>
+					        <p className="font-dmsans text-lg md:text-xl mt-2 ">Quil is a website where employees with mental health issues can get help and also download our work-life balance app</p>
+					    </a>
+					</motion.div>
+				</motion. div>
+				<motion.div 
+				    variants= {containerVariants}
+				    initial="hidden"
+				    animate="visible"
+				    whileHover="hover"
+				    whileInView="scroll"
+				    className="text-center flex justify-center items-center text-center rounded-lg h-[300px] lg:h-[400px] w-[300px] lg:w-[500px] bg-comfy bg-center bg-no-repeat bg-cover"
+				>
+					<motion.div
+					    variants={hoverVariants}
+					    initial="hidden"
+				        whileHover="hover"
+				        className="p-4 w-full h-full grid place-items-center text-very-dark-grayish-bluee font-bold"
+				        style={{backdropFilter: 'blur(10px)'}}
+					>
+					    <a target="_blank" rel="noreferrer" href="https://comfyproperty.com.ng/">
+					        <h2 className="font-montserrat text-2xl md:text-3xl lg:text-5xl">Comfy Properties Limited</h2>
+					        <p className="font-dmsans text-lg md:text-xl mt-2 ">A real estate website built for a client</p>
+					    </a>
+					</motion.div>
+				</motion. div>
+				<motion.div 
+				    variants= {containerVariants}
+				    initial="hidden"
+				    animate="visible"
+				    whileHover="hover"
+				    whileInView="scroll"
+				    className="text-center flex justify-center items-center text-center rounded-lg h-[300px] lg:h-[400px] w-[300px] lg:w-[500px] bg-black bg-center bg-no-repeat bg-cover"
+				>
+					<motion.div
+					    variants={hoverVariants}
+					    initial="hidden"
+				        whileHover="hover"
+				        className="p-4 w-full h-full grid place-items-center text-very-dark-grayish-bluee font-bold"
+				        style={{backdropFilter: 'blur(10px)'}}
+					>
+					    <a target="_blank" rel="noreferrer" href="https://www.joinblacktechies.com/">
+					        <h2 className="font-montserrat text-2xl md:text-3xl lg:text-5xl">Black Techies</h2>
+					        <p className="font-dmsans text-lg md:text-xl mt-2 ">Black techies Waitlist landing page</p>
+					    </a>
+					</motion.div>
+				</motion. div>
+				<motion.div 
+				    variants= {containerVariants}
+				    initial="hidden"
+				    animate="visible"
+				    whileHover="hover"
+				    whileInView="scroll"
+				    className="text-center flex justify-center items-center text-center rounded-lg h-[300px] lg:h-[400px] w-[300px] lg:w-[500px] bg-tutcov bg-center bg-no-repeat bg-cover"
+				>
+					<motion.div
+					    variants={hoverVariants}
+					    initial="hidden"
+				        whileHover="hover"
+				        className="p-4 w-full h-full grid place-items-center text-very-dark-grayish-bluee font-bold"
+				        style={{backdropFilter: 'blur(10px)'}}
+					>
+					    <a target="_blank" rel="noreferrer" href="https://tutcov.vercel.app/dashboard">
+					        <h2 className="font-montserrat text-2xl md:text-3xl lg:text-5xl">Tutcov dashboard</h2>
+					        <p className="font-dmsans text-lg md:text-xl mt-2 ">in progress...</p>
 					    </a>
 					</motion.div>
 				</motion. div>
